d5cb1fc8978f4cf376d97aa82cec20ecd73af1e9
skovhus <kenneth.skovhus@gmail.com>
2017-03-27 00:53:19 +0100

    Round plugin without working room concept...

--- src/index.server.js
+++ src/index.server.js
@@ -2,54 +2,11 @@ const Moniker = require('moniker')
 const plugplay = require('plugplay/server')
 const playersPluginFactory = require('plugplay/plugins/players/server')
 const roomsPluginFactory = require('plugplay/plugins/rooms/server')
+const roundPlugin = require('./server/round-plugin')
 
 var names = Moniker.generator([Moniker.noun])
 
-const getFreshRound = () => {
-  return {
-    round: {
-      options: [
-        {
-          id: 0,
-          label: names.choose(),
-          isAnswered: true,
-          answeredBy: 123456789
-        },
-        {
-          id: 1,
-          label: names.choose()
-        },
-        {
-          id: 2,
-          label: names.choose()
-        },
-        {
-          id: 3,
-          label: names.choose()
-        }
-      ],
-      correctOption: Math.floor(Math.random() * 4)
-    }
-  }
-}
-
 const roomReducer = (state, action) => {
-  if (!state.isReady) {
-    return state.data
-  }
-
-  if (!state.data || (action.type === 'USER_ACTION' && action.payload.type === 'rematch')) {
-    return getFreshRound()
-  }
-
-  if (action.type === 'USER_ACTION' && action.payload.type === 'option select') {
-    // FIXME: first one to find the right one, and you can only vote once
-    // const nextBoard = [...state.data.board]
-    // nextBoard[action.payload.data] = action.payload.playerId
-
-    return getFreshRound()
-  }
-
   return state.data
 }
 
@@ -64,19 +21,12 @@ const roomsPlugin = roomsPluginFactory({
 const mapStateToClientProps = (state, { playerId, roomId }) => {
   const room = state.rooms.byId[roomId]
 
-  if (roomId && room.data) {
-    return {
-      screen: 'round-selection',
-      round: room.data.round,
-      players: room.players,
-      isReady: room.isReady,
-      roomId
-    }
-  }
+  const round = state['round-plugin']
 
   if (roomId) {
     return {
-      screen: 'room-selection',
+      screen: 'round',
+      round,
       players: room.players,
       isReady: room.isReady,
       roomId
@@ -84,11 +34,12 @@ const mapStateToClientProps = (state, { playerId, roomId }) => {
   }
 
   return {
+    screen: 'room-selection',
     rooms: state.rooms.ids.map(roomId => state.rooms.byId[roomId])
   }
 }
 
 plugplay({
   mapStateToClientProps,
-  plugins: [playersPlugin, roomsPlugin]
+  plugins: [playersPlugin, roomsPlugin, roundPlugin]
 })
